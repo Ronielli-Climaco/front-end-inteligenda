4fd8266424f21371acbc44fd3f9b8b6c62b3c82b
Seu Nome <seu@email.com>
2025-10-22 22:18:07 -0300

    2

--- agendamento.js
+++ agendamento.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector(".agendamento-form");
     const mensagem = document.getElementById("mensagem");
+    const telInput = document.getElementById("tel-cliente");
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
@@ -8,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function() {
         const barbeiro = form.Barbeiro.value;
         const data = form['data-selecionada'].value;
         const horario = form['horario-selecionado'].value;
+
+        
 
         const confirmar = confirm(`Deseja confirmar o agendamento com ${barbeiro} em ${data} às ${horario}?`);
         if (!confirmar) {
@@ -18,7 +21,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
         mensagem.innerHTML = "Agendamento confirmado!";
         mensagem.style.color = "green";
-        mensagem.style.textAlign = "center";
-
+        mensagem.style.textAlign = "center";    
     })
+
+    telInput.addEventListener("input", function() { 
+        let valor = this.value.replace(/\D/g, ''); // Remove o que não é número.
+
+        if (valor.length > 11) {
+            valor = valor.slice(0, 11); // limita a 11 dígitos
+        }
+
+          // Formatação: (XX) X XXXX XXXX
+        if (valor.length > 7) {
+            this.value = `(${valor.slice(0, 2)}) ${valor.slice(2, 3)} ${valor.slice(3, 7)} ${valor.slice(7)}`;
+        } else if (valor.length > 3) {
+            this.value = `(${valor.slice(0, 2)}) ${valor.slice(2, 3)} ${valor.slice(3)}`;
+        } else if (valor.length > 2) {
+            this.value = `(${valor.slice(0, 2)}) ${valor.slice(2)}`;
+        } else {
+            this.value = valor;
+        }
+
+    });
 })
+
